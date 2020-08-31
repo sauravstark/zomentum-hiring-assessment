58a5535cac0ec83893e3dd82ac890380bc2392b3
Saurav Stark <>
2020-08-31 07:48:13 +0530

    Create Endpoint to update ticket timing

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -18,24 +18,24 @@ exports.book = (req, res) => {
     if (errors.length > 0) {
         res.send({
             status: "ERR",
-            messgae: errors,
+            message: errors,
         });
     } else {
         TimingModel.findOne({ movie: req.body.movie, time: req.body.time }, (err, timing) => {
             if (err) {
                 res.send({
                     status: "ERR",
-                    messgae: "Could not book ticket",
+                    message: "Could not book ticket",
                 });
             } else if (timing == null) {
                 res.send({
                     status: "ERR",
-                    messgae: "Movie not found",
+                    message: "Movie not found",
                 });
             } else if (timing.capacity == 0) {
                 res.send({
                     status: "ERR",
-                    messgae: "No seats available",
+                    message: "No seats available",
                 });
             } else {
                 let doc = {
@@ -48,7 +48,7 @@ exports.book = (req, res) => {
                     if (err) {
                         res.send({
                             status: "ERR",
-                            messgae: "Could not book ticket",
+                            message: "Could not book ticket",
                         });
                     } else {
                         TimingModel.findByIdAndUpdate(timing._id, { capacity: (timing.capacity - 1) }, (err) => {
@@ -58,11 +58,100 @@ exports.book = (req, res) => {
                         });
                         res.send({
                             status: "OK",
-                            messgae: "Ticket Booked",
+                            message: "Ticket Booked",
                             ticket: doc,
                         });
                     }
                 });
+            }
+        })
+    }
+};
+
+exports.update = (req, res) => {
+    let errors = [];
+    if (typeof (req.body.name) !== 'string' || req.body.name.length < 3) {
+        errors.push("Invalid User Name");
+    }
+    if (typeof (req.body.contact) !== 'string' || !req.body.contact.match(/^\d{10}$/)) {
+        errors.push("Invalid Contact");
+    }
+    if (typeof (req.body.movie) != 'string') {
+        errors.push("Invalid Movie Name")
+    }
+    if (typeof (req.body.old_time) != 'string' || Date.parse(req.body.old_time) === NaN) {
+        errors.push("Invalid Old Time");
+    }    
+    if (typeof (req.body.new_time) != 'string' || Date.parse(req.body.new_time) === NaN) {
+        errors.push("Invalid New Time");
+    }
+    if (errors.length > 0) {
+        res.send({
+            status: "ERR",
+            message: errors,
+        });
+    } else {
+        TimingModel.find({ movie: req.body.movie }, (err, timings) => {
+            if (err) {
+                res.send({
+                    status: "ERR",
+                    message: "Could not complete request",
+                });
+            } else if (timings == null) {
+                res.send({
+                    status: "ERR",
+                    message: "Movie not found",
+                });
+            } else {
+                let old_timing = timings.find(el => Date.parse(el.time) === Date.parse(req.body.old_time));
+                let new_timing = timings.find(el => Date.parse(el.time) === Date.parse(req.body.new_time));
+
+                if (old_timing === undefined) {
+                    res.send({
+                        status: "ERR",
+                        message: "Old Time not found",
+                    });
+                } else if (new_timing === undefined){
+                    res.send({
+                        status: "ERR",
+                        message: "New Time not found",
+                    });
+                } else if (new_timing.capacity == 0) {
+                    res.send({
+                        status: "ERR",
+                        message: "No Vacancy in new time",
+                    });
+                } else {
+                    TicketModel.findOne({ user_name: req.body.name, user_contact: req.body.contact, timing: old_timing._id }, (err, doc) => {
+                        if (err) {
+                            console.log(err);
+                        } else if (doc == null) {
+                            res.send({
+                                status: "ERR",
+                                message: "Booking not found",
+                            });
+                        } else {
+                            TimingModel.findByIdAndUpdate(old_timing._id, { capacity: (old_timing.capacity + 1) }, (err) => {
+                                if (err)
+                                    console.log(err);
+                            });
+                            TimingModel.findByIdAndUpdate(new_timing._id, { capacity: (new_timing.capacity - 1) }, (err) => {
+                                if (err)
+                                    console.log(err);
+                            });
+                            TicketModel.findByIdAndUpdate(doc._id, { timing: new_timing._id }, {new: true}, (err, out) => {
+                                if (err)
+                                    console.log(err);
+                                else
+                                    res.send({
+                                        status: "OK",
+                                        message: "Timing updated",
+                                        updated: out
+                                    });
+                            });
+                        }
+                    })
+                }
             }
         })
     }
--- routes/ticket.js
+++ routes/ticket.js
@@ -4,5 +4,6 @@ const TicketController = require("../controllers/TicketController");
 var router = express.Router();
 
 router.post("/book/", TicketController.book);
+router.post("/update/", TicketController.update);
 
 module.exports = router;
